89946874d38b3edf700c65aa2c3a442572ed9c9b
likemalbolge <vanyaolenyshak@gmail.com>
2024-12-16 17:26:05 +0200

    added datetime rangefilter

--- access_table/views.py
+++ access_table/views.py
@@ -23,7 +23,7 @@ def add_record_to_db(data):
 
 def fetch_all_records():
     today = datetime.now().strftime('%Y-%m-%d')
-    start_time = f'2024-12-01T08:00:00+02:00'
+    start_time = f'{today}T08:00:00+02:00'
     end_time = f'{today}T21:00:00+02:00'
 
     url = 'http://192.168.8.39/ISAPI/AccessControl/AcsEvent?format=json'
